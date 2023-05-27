1e11a9762f2b36bceabf1519358f3b9771c2c87e
Лилит <@LAPTOP-3EJS7JBS>
2023-05-27 22:48:31 +0300

    Добавьте файлы проекта.

---
+++ HW_tema14_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//генерация температуры с условиями.
+
+Random random = new Random(); //создаем экземпляр класса рандом
+
+int temperature = 0; //переменная, в которую будут записываться значения температуры
+
+for(int i=1;  i<=100; i++)
+{
+    temperature = random.Next(1, 101); //генерируем температуру от 1 до 100
+
+    if(temperature<90) Console.WriteLine(temperature+" градуса(ов) - входит в норму ");
+
+    else if (temperature==90) Console.WriteLine(temperature+ " градуса(ов) - кристическая температура ");
+
+    else
+    {
+        Console.WriteLine(temperature+ " градуса(ов) - критическая температура");
+        break; //прерываем программу
+    }
+}
